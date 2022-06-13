1672340dffb870cb0d41b7266e1d40d8afd8384b
Supakorn <supakorn0728@gmail.com>
2022-06-13 21:24:19 +0700

    initial

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 
+const HeaderContainer = styled.div``;
 const Header = () => {
-  return <div>Header</div>;
+  return <HeaderContainer>Header</HeaderContainer>;
 };
 
 export default Header;
