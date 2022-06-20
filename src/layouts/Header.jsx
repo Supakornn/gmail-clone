cd08f995b80fcd0a034e29af4454a69c8da4b6e0
Supakorn <supakorn0728@gmail.com>
2022-06-20 21:53:01 +0700

    initial

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    object-fit: contain;
+    height: 80px;
+    margin-left: 5px;
+  }
+`;
+
+const HeaderMiddle = styled.div``;
+
+const HeaderRight = styled.div``;
+
 const Header = () => {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202010/Google_Gmail_New_Logo_India_To_1200x768.jpeg?WgdQ3Tx7r4ZssTpgfxm1Iwb5KMAG8S4A&size=770:433"
+          alt="Logo"
+        />
+      </HeaderLeft>
+      <HeaderMiddle>
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <FormatAlignLeftIcon />
+      </HeaderMiddle>
+      <HeaderRight></HeaderRight>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
