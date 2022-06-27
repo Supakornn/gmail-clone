490666080a5ffebf3f6a57ca1e4e4826dd4779b6
Supakorn <supakorn0728@gmail.com>
2022-06-27 20:46:32 +0700

    initial commit

---
+++ src/components/SidebarOption.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const SidebarOptionContainer = styled.div``;
+
+const SidebarOption = ({ Icon, Title, number }) => {
+  return <SidebarOptionContainer></SidebarOptionContainer>;
+};
+
+export default SidebarOption;
--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from "@mui/icons-material/Inbox";
 
-const SidebarContainer = styled.div``;
+const SidebarContainer = styled.div`
+  .sidebar__compose {
+    margin-top: 15px !important;
+    margin-left: 10px !important;
+    margin-bottom: 15px !important;
+    text-transform: capitalize !important;
+    color: gray;
+    padding: 15px !important;
+    border-radius: 30px !important;
+    box-shadow: 0 2px 5px -2px rgba(0, 0, 0, 0.75);
+  }
+`;
 
 const Sidebar = () => {
-  return <SidebarContainer>Sidebar</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <Button startIcon={<AddIcon fontSize="large" />} className="sidebar__compose">
+        compose
+      </Button>
+      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+    </SidebarContainer>
+  );
 };
 
 export default Sidebar;
