89411da720d722ece36080b7f63253d100d3e582
Supakorn <supakorn0728@gmail.com>
2022-06-30 21:38:15 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./layouts/Header";
 import Sidebar from "./layouts/Sidebar";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 const AppContainer = styled.div``;
 
+const BodyContainer = styled.div``;
+
 const App = () => {
   return (
-    <AppContainer>
-      <Header />
-      <Sidebar />
-    </AppContainer>
+    <Router>
+      <AppContainer>
+        <Header />
+        <BodyContainer>
+          <Sidebar />
+          <Routes></Routes>
+        </BodyContainer>
+      </AppContainer>
+    </Router>
   );
 };
 
