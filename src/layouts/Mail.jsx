b8e258051612c3706d05144df19de9cdb42f4f0a
Supakorn <supakorn0728@gmail.com>
2022-10-07 15:47:06 +0700

    initial commit

--- src/layouts/Mail.jsx
+++ src/layouts/Mail.jsx
@@ -1,8 +1,73 @@
 import React from "react";
 import styled from "styled-components";
+import { IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MoveToInboxIcon from "@mui/icons-material/MoveToInbox";
+import ErrorIcon from "@mui/icons-material/Error";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EmailIcon from "@mui/icons-material/Email";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const MailContainer = styled.div``;
+
+const MailTools = styled.div``;
+
+const MailToolsLeft = styled.div``;
+
+const MailToolsRight = styled.div``;
 
 const Mail = () => {
-  return <div>Mail</div>;
+  const navigate = useNavigate();
+
+  return (
+    <MailContainer>
+      <MailTools>
+        <MailToolsLeft>
+          <IconButton onClick={() => navigate("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </MailToolsLeft>
+
+        <MailToolsRight></MailToolsRight>
+      </MailTools>
+    </MailContainer>
+  );
 };
 
 export default Mail;
