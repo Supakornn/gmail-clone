f23110b66654ccc2687704e532e1b475886027b4
Supakorn <supakorn0728@gmail.com>
2022-10-10 05:20:37 +0700

    initial commit

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import CloseIcon from "@mui/icons-material/Close";
 import { Button } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const SendMailContainer = styled.div`
   position: absolute;
@@ -17,6 +18,24 @@ const SendMailContainer = styled.div`
   flex-direction: column;
   border: 1px solid whitesmoke;
   box-shadow: 0px 5px 7px 0px rgba(0, 0, 0, 0.24);
+
+  form {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+
+  > form > input {
+    height: 30px;
+    padding: 10px;
+    border: none;
+    border-bottom: 1px solid whitesmoke;
+    outline: none;
+  }
+
+  .sendMail_message {
+    flex: 1;
+  }
 `;
 
 const SendMailHeader = styled.div`
@@ -30,11 +49,23 @@ const SendMailHeader = styled.div`
     color: whitesmoke;
     font-size: 13px;
   }
+
+  .sendMail_close {
+    cursor: pointer;
+  }
 `;
 
-const SendMailOptions = styled.div``;
+const SendMailOptions = styled.div`
+  .sendMail_send {
+    background-color: #3079ed !important;
+    text-transform: capitalize !important;
+    margin: 15px !important;
+  }
+`;
 
 const Sendmail = () => {
+  const { register, handleSubmit, watch, errors } = useForm();
+
   return (
     <SendMailContainer>
       <SendMailHeader>
@@ -43,12 +74,14 @@ const Sendmail = () => {
       </SendMailHeader>
 
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <input name="to" type="text" placeholder="To" />
+        <input name="subject" type="text" placeholder="Subject" />
+        <input name="text" type="text" placeholder="Message..." className="sendMail_message" />
 
         <SendMailOptions>
-          <Button>Send</Button>
+          <Button className="sendMail_send" variant="contained" color="primary" type="submit">
+            Send
+          </Button>
         </SendMailOptions>
       </form>
     </SendMailContainer>
